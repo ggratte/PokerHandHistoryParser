fe4bca2ad0bb4141ff90c872d27cb6a32ef45e32
RZ <r82zhang@edu.uwaterloo.ca>
2023-11-26 20:22:48 -0500

    Update BoardCards with efficient equality compare function + Unit tests

--- HandHistories.Objects/Cards/BoardCards.cs
+++ HandHistories.Objects/Cards/BoardCards.cs
@@ -80,92 +80,77 @@ namespace HandHistories.Objects.Cards
             }
         }
 
-        public Card GetTurnCard()
+        public bool EqualsViaHoldemOmahaRule(BoardCards other)
         {
-            switch (this.Count)
+            if (other == null) return false;
+            if (other.Cards.Count != this.Cards.Count) return false;
+
+            switch (Cards.Count)
             {
+                case 5: // River
+                    if (Cards[4] != other.Cards[4])
+                    {
+                        return false;
+                    }
+                    goto case 4;
+                case 4: // Turn
+                    if (Cards[3] != other.Cards[3])
+                    {
+                        return false;
+                    }
+                    goto case 3;
+                case 3: // Flop
+                    ulong thismask = 1ul << this.Cards[0].CardIntValue
+                                   | 1ul << this.Cards[1].CardIntValue
+                                   | 1ul << this.Cards[2].CardIntValue;
+
+                    ulong othermask = 1ul << other.Cards[0].CardIntValue
+                                    | 1ul << other.Cards[1].CardIntValue
+                                    | 1ul << other.Cards[2].CardIntValue;
+
+                    return thismask == othermask;
+                case 0: // Preflop
+                    return true;
+
+                default: throw new ArgumentException("Unknown number of board cards " + Cards.Count);
+            }
+        }
+
+        public bool EqualsViaCourchevelRule(BoardCards other)
+        {
+            if (other == null) return false;
+            if (other.Cards.Count != this.Cards.Count) return false;
+
+            switch (Cards.Count)
+            {
+                case 5: // River
+                    if (Cards[4] != other.Cards[4])
+                    {
+                        return false;
+                    }
+                    goto case 4;
+                case 4: // Turn
+                    if (Cards[3] != other.Cards[3])
+                    {
+                        return false;
+                    }
+                    goto case 3;
+                case 3: // Flop
+                    ulong thismask = 1ul << this.Cards[1].CardIntValue
+                                   | 1ul << this.Cards[2].CardIntValue;
+
+                    ulong othermask = 1ul << other.Cards[1].CardIntValue
+                                    | 1ul << other.Cards[2].CardIntValue;
+
+                    if (thismask != othermask) return false;
+                    goto case 1;
+                case 1: // Preflop
+                    return Cards[0] == other.Cards[0];
                 case 0:
-                case 3:
-                    throw new Exception("Turn card does not exist on this board!");
-                case 4:
-                case 5:
-                    return this[3];
-                default:
-                    throw new ArgumentException("Unknown number of board cards " + Cards.Count);
+                    return true;
 
+                default: throw new ArgumentException("Unknown number of board cards " + Cards.Count);
             }
-        }
-
-        public Card GetRiverCard()
-        {
-            switch (this.Count)
-            {
-                case 0:
-                case 3:
-                case 4:
-                    throw new Exception("River card does not exist on this board!");
-                case 5:
-                    return this[4];
-                default:
-                    throw new ArgumentException("Unknown number of board cards " + Cards.Count);
-            }
-        }
-
-        /**
-         * When comparing two boards, order matters. The same 5 cards, if come to the board with different order, should be considerred as different
-         * boards. Therefore, when comparing two boards, we should firstly compare Flop as a group, and then compare Turn and River respecticely.
-         */
-        public override bool Equals(object obj)
-        {
-            bool stringEquality = obj.ToString().Equals(ToString());
-            if (stringEquality) return true;
-
-            BoardCards boardGroup = obj as BoardCards;
-
-            if (boardGroup == null) return false;
-
-            if (this.Count != boardGroup.Count) return false;
-            if (this.Count == 0) return true;
-
-            // if this is just comparing flops, we can use the base compartor.
-            if (this.Count == 3)
-            {
-                return base.Equals(boardGroup);
-            }
-
-            // because the order matters, we need to compare flop first, and then compare turn and river.
-            if (this.Count == 4)
-            {
-                return this.GetBoardOnStreet(Street.Flop).Equals(boardGroup.GetBoardOnStreet(Street.Flop)) && this.GetTurnCard().Equals(boardGroup.GetTurnCard());
-            }
-
-            if (this.Count == 5)
-            {
-                return this.GetBoardOnStreet(Street.Turn).Equals(boardGroup.GetBoardOnStreet(Street.Turn)) && this.GetRiverCard().Equals(boardGroup.GetRiverCard());
-            }
-
-            throw new ArgumentException("Unknown number of board cards " + Cards.Count);
-        }
-
-        public static bool operator ==(BoardCards b1, BoardCards b2)
-        {
-            if (ReferenceEquals(b1, b2))
-            {
-                return true;
-            }
-            else if (ReferenceEquals(b1, null) || ReferenceEquals(b2, null))
-            {
-                return false;
-            }
-
-            return b1.Equals(b2);
-        }
-
-        public static bool operator !=(BoardCards b1, BoardCards b2) { return !(b1 == b2); }
-
-        public override int GetHashCode()
-        {
-            return (Cards != null ? Cards.GetHashCode() : 0);
         }
     }
 }
