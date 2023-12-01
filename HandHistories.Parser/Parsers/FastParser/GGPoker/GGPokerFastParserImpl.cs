752869768ec169584244b0b92f8715918be2a45b
RZ <r82zhang@edu.uwaterloo.ca>
2023-11-30 22:53:21 -0500

    Parse paying Insurance fee

--- HandHistories.Parser/Parsers/FastParser/GGPoker/GGPokerFastParserImpl.cs
+++ HandHistories.Parser/Parsers/FastParser/GGPoker/GGPokerFastParserImpl.cs
@@ -685,7 +685,7 @@ namespace HandHistories.Parser.Parsers.FastParser.GGPoker
 
                 // xyz: Receives Cashout ($9.98)
                 case ')':
-                    if (line.Contains("Receives Cashout"))
+                    if (line.Contains("Cashout"))
                     {
                         break;
                     }
@@ -706,6 +706,7 @@ namespace HandHistories.Parser.Parsers.FastParser.GGPoker
                 default:
                     if (colonIndex > -1 && !line.Contains("Receives Cashout"))
                     {
+                        Console.WriteLine(line);
                         handActions.Add(ParseRegularActionLine(line, colonIndex, currentStreet));
                     }
                     else
@@ -775,6 +776,15 @@ namespace HandHistories.Parser.Parsers.FastParser.GGPoker
                     amount = actionLine.Substring(firstDigitIndex, actionLine.Length - firstDigitIndex).ParseAmount();
                     actionType = HandActionType.RAISE;
                     break;
+                
+                //xyz: Pays Cashout Risk ($22.68)
+                case 'P':
+                    firstDigitIndex = actionLine.LastIndexOf(' ') + 2;
+                    amount = actionLine.Substring(firstDigitIndex, actionLine.Length - firstDigitIndex-1).ParseAmount();
+                    actionType = HandActionType.PAYS_INSURANCE_FEE;
+                    currentStreet = Street.Showdown;
+                    break;
+
                 default:
                     throw new HandActionException(actionLine, "ParseRegularActionLine: Unrecognized line:" + actionLine);
             }
